43a23d20036bed6c54b6daf60eabd2f5b346140a
amiroof54 <amiroo.f54@gmail.com>
2024-05-11 20:12:36 +0330

    Restructered the dns selection code. for easier readability and adding of new DNSs.

--- dnsch.sh
+++ dnsch.sh
@@ -22,6 +22,17 @@ if [[ $# -lt 1 ]]; then
     usage
 fi
 
+# A list of all DNSs
+declare -A dnsList
+dnsList["g",0]="8.8.8.8"; dnsList["g",1]="8.8.4.4"
+dnsList["sh",0]="178.22.122.100"; dnsList["sh",1]="185.51.200.2"
+dnsList["ag",0]="176.103.130.130"; dnsList["ag",1]="176.103.130.131"
+dnsList["cf",0]="1.1.1.1"; dnsList["cf",1]="1.0.0.1"
+dnsList["403",0]="10.202.10.202"; dnsList["403",1]="10.202.10.102"
+dnsList["bg",0]="185.55.226.26"; dnsList["bg",1]="185.55.225.25"
+dnsList["rd",0]="10.202.10.10"; dnsList["rd",1]="10.202.10.11"
+dnsList["el",0]="78.157.42.100"; dnsList["el",1]="78.157.42.101"
+
 # Parse the arguments
 if [[ $1 == "-s" || $1 == "--set" ]]; then
     # Check if exactly two IP addresses are provided
@@ -33,37 +44,13 @@ if [[ $1 == "-s" || $1 == "--set" ]]; then
 elif [[ $1 == "-c" || $1 == "--clear" ]]; then
     nameservers=()
 else
-    # Define the nameservers to add based on input argument
-    case $1 in
-        g)
-            nameservers=("nameserver 8.8.8.8" "nameserver 8.8.4.4")
-            ;;
-        sh)
-            nameservers=("nameserver 178.22.122.100" "nameserver 185.51.200.2")
-            ;;
-        ag)
-            nameservers=("nameserver 176.103.130.130" "nameserver 176.103.130.131")
-            ;;
-        cf)
-            nameservers=("nameserver 1.1.1.1" "nameserver 1.0.0.1")
-            ;;
-        403)
-            nameservers=("nameserver 10.202.10.202" "nameserver 10.202.10.102")
-            ;;
-        bg)
-            nameservers=("nameserver 185.55.226.26" "nameserver 185.55.225.25")
-            ;;
-        rd)
-            nameservers=("nameserver 10.202.10.10" "nameserver 10.202.10.11")
-            ;;
-        el)
-            nameservers=("nameserver 78.157.42.100" "nameserver 78.157.42.101")
-            ;;
-        *)
-            echo "Invalid option: $1"
-            usage
-            ;;
-    esac
+    # Add the nameservers based on the previous list
+    nameservers=("nameserver ${dnsList[$1,0]}" "nameserver ${dnsList[$1,1]}")
+    # If the dns wasn't in the list, throw an error
+    if [ "${nameservers[0]}" == "nameserver " ]; then
+        echo "Invalid option: $1"
+        usage
+    fi
 fi
 
 # Remove all existing nameservers and add new ones atomically
